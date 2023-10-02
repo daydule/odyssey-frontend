3dbedaf568e904a7cd25dcd3d493f5fb8968e5d4
Keisuke Nemura <nemuraystk@gmail.com>
2023-10-02 13:05:51 +0000

    add ODY-26 画像つき説明カード実装＆ストーリーブック実装

---
+++ src/components/leaf/AboutCardWithImage.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { AboutCardWithImage } from './AboutCardWithImage';
+
+const meta: Meta<typeof AboutCardWithImage> = {
+  component: AboutCardWithImage,
+  title: 'Leaf/AboutCardWithImage',
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+/**
+ * 画像(左側)ありの説明カード
+ */
+export const Default: Story = {
+  args: {
+    imagePath: '/images/icon_negate.jpeg',
+    title: 'Time is Money',
+    text: 'テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト',
+    isImageLeft: true,
+  },
+};
+
+/**
+ * 画像(右側)ありの説明カード
+ */
+export const RightImage: Story = {
+  args: {
+    imagePath: '/images/icon_negate.jpeg',
+    title: 'Time is Money',
+    text: 'テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト',
+    isImageLeft: false,
+  },
+};
+
+/**
+ * 画像(右側)ありの説明カード
+ */
+export const NoImage: Story = {
+  args: {
+    imagePath: '/images/xxx.jpeg',
+    title: 'Time is Money',
+    text: 'テストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテストテスト',
+    isImageLeft: false,
+  },
+};
---
+++ src/components/leaf/AboutCardWithImage.tsx
@@ -0,0 +1,41 @@
+type Props = {
+  imagePath: string;
+  title: string;
+  text: string;
+  isImageLeft: boolean;
+};
+
+export const AboutCardWithImage = (props: Props) => {
+  const getImageArea = () => {
+    return (
+      <div className='w-1/2 text-center' data-testid='AboutCardWithImage_ImageArea'>
+        <img className='h-full w-full rounded-[50px] bg-gray-300 object-cover' src={props.imagePath} alt='説明画像' />
+      </div>
+    );
+  };
+  const getTextArea = () => {
+    return (
+      <div className='w-1/2 p-12 text-center' data-testid='AboutCardWithImage_TextArea'>
+        <p className='text-xl'>{props.title}</p>
+        <p className='mt-12 text-lg'>{props.text}</p>
+      </div>
+    );
+  };
+  const getRenderComponent = () => {
+    return props.isImageLeft ? (
+      <>
+        {getImageArea()}
+        {getTextArea()}
+      </>
+    ) : (
+      <>
+        {getTextArea()}
+        {getImageArea()}
+      </>
+    );
+  };
+
+  return (
+    <div className='flex h-[384px] w-full rounded-[50px] border shadow-lg lg:w-[1024px]'>{getRenderComponent()}</div>
+  );
+};
