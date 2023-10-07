d4022db3ddcc7fe27ee61b8231b1c68f65ae783e
Keisuke Nemura <nemuraystk@gmail.com>
2023-10-07 13:57:17 +0000

    add ODY-220 ヘッダーコンポーネント実装

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@/components/leaf/Header';
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header />
+      <div>about page</div>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,12 @@
+import { Header } from '@/components/leaf/Header';
+
+const MainPage = () => {
+  return (
+    <div>
+      <Header />
+      <div>main page</div>
+    </div>
+  );
+};
+
+export default MainPage;
