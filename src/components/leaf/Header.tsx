3049423d10752bf48a839f3389a1f6839f3a1e56
Keisuke Nemura <nemuraystk@gmail.com>
2023-10-13 16:41:23 +0000

    fix ODY-220 色定義削除、スタイル調整

--- src/components/leaf/Header.tsx
+++ src/components/leaf/Header.tsx
@@ -3,13 +3,13 @@ import { useRouter } from 'next/router';
 export const Header = () => {
   const router = useRouter();
   return (
-    <div className='flex w-full items-center bg-light-gray py-4'>
+    <div className='flex h-20 w-full items-center bg-gray-100 py-4'>
       <div
-        className='mx-4 cursor-pointer font-medium md:mx-8'
+        className='mx-4 h-20 cursor-pointer font-medium md:mx-8'
         onClick={() => router.push('/main')}
         data-testid='Header_Logo'
       >
-        <img src={'/images/header_logo.png'} alt='ヘッダーロゴ画像' />
+        <img className='h-full w-auto object-contain' src={'/images/header_logo.png'} alt='ヘッダーロゴ画像' />
       </div>
       <a
         className='ml-auto mr-4 cursor-pointer font-medium md:mr-8'
