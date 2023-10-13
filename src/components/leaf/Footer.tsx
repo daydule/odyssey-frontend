d7441763b160da2f9d70798f7a0a5c6c7f7cb002
Keisuke Nemura <nemuraystk@gmail.com>
2023-10-13 16:55:41 +0000

    update ODY-224 スタイル調整

--- src/components/leaf/Footer.tsx
+++ src/components/leaf/Footer.tsx
@@ -1,3 +1,3 @@
 export const Footer = () => {
-  return <div className='w-full bg-light-gray p-1 text-center'>&copy; 2023 Habitat Hub</div>;
+  return <div className='w-full bg-gray-100 p-1 text-center'>&copy; 2023 Habitat Hub</div>;
 };
