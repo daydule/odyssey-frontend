6e7b2a56917e35d2a103b263472a1021e6fd80ea
Keisuke Nemura <nemuraystk@gmail.com>
2023-10-21 04:23:02 +0000

    add 商品一覧表示カードの実装

---
+++ src/components/tree/MainCardWithCommodityResult.tsx
@@ -0,0 +1,36 @@
+import { useMemo } from 'react';
+import CommodityCard, { type Commodity } from '../leaf/CommodityCard';
+import MainCard from './MainCard';
+
+type Props = {
+  commodities: Commodity[];
+};
+
+const MainCardWithCommodityResult = ({ commodities }: Props) => {
+  const displayCommodities = useMemo(() => {
+    return (
+      <ul className='box-content h-full w-full overflow-y-scroll pr-4'>
+        {commodities.map((commodity, index) => {
+          return (
+            <li className='[&:not(:first-child)]:mt-4' key={`commodity_${index}`}>
+              <CommodityCard
+                imagePath={commodity.imagePath}
+                altText={commodity.altText}
+                title={commodity.title}
+                price={commodity.price}
+              ></CommodityCard>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }, [commodities]);
+
+  return (
+    <MainCard title='購入可能商品' headerBgColor='bg-yellow-200'>
+      <div className='h-full w-full overflow-hidden'>{displayCommodities}</div>
+    </MainCard>
+  );
+};
+
+export default MainCardWithCommodityResult;
