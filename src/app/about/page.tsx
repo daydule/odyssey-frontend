30fd66dfe8c76901cb20b9b81535680d9eda12c6
Kota Kawamoto <atok.reverse.1040@gmail.com>
2023-12-13 04:31:43 +0900

    ODY-276 create Aboutページを追加

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import { About } from '@/components/forest/About';
+
+export default function Page() {
+  return (
+    <div className='w-full bg-about-page-image bg-cover bg-no-repeat py-10'>
+      <About />
+    </div>
+  );
+}
