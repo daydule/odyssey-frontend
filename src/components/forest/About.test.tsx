e59e8e0a343af63d35065c9aca5ad45ef67b0fa3
Kota Kawamoto <atok.reverse.1040@gmail.com>
2023-12-17 01:45:10 +0000

    ODY-276 fix テストケース修正

--- src/components/forest/About.test.tsx
+++ src/components/forest/About.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen, fireEvent, getAllByRole } from '@testing-library/react';
 import { About } from './About';
 
+const setWindowSize = (width: number, height: number) => {
+  window.innerWidth = width;
+  window.innerHeight = height;
+  window.dispatchEvent(new Event('resize'));
+};
+
 describe('Aboutページのforestコンポーネントのテスト', () => {
-  it('画像付き説明テキストが複数あるとき、奇数番目は左に、偶数番目は右に画像が表示されること', () => {
+  it('画面幅が769px以上の場合、奇数番目は左に、偶数番目は右に画像が表示されること', () => {
+    setWindowSize(769, 900);
+
     render(<About />);
 
     const aboutCardWithImages = screen.getAllByTestId('AboutCardWithImage');
@@ -19,5 +27,24 @@ describe('Aboutページのforestコンポーネントのテスト', () => {
 
     // 偶数番目は右に画像
     expect(divDomsInCard[1][1].dataset.testid).toBe('AboutCardWithImage_ImageArea');
+  });
+
+  it('画面幅が769px未満の場合、全て右に画像が表示されること', () => {
+    setWindowSize(768, 900);
+
+    render(<About />);
+
+    const aboutCardWithImages = screen.getAllByTestId('AboutCardWithImage');
+    const divDomsInCard = aboutCardWithImages.map((card) => {
+      return getAllByRole(card, 'generic');
+    });
+
+    // カードは3つ
+    expect(divDomsInCard.length).toBe(3);
+
+    // 全て右に画像
+    expect(divDomsInCard[0][0].dataset.testid).toBe('AboutCardWithImage_ImageArea');
+    expect(divDomsInCard[1][0].dataset.testid).toBe('AboutCardWithImage_ImageArea');
+    expect(divDomsInCard[2][0].dataset.testid).toBe('AboutCardWithImage_ImageArea');
   });
 });
