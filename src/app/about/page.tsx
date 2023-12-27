d4c93b6fb1dc8221548e8d975103c44ede05180c
tom-takeru <tom.2461.ty@gmail.com>
2023-12-27 16:38:11 +0000

    ODY-228 Update layout and about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,6 @@
+// src/app/about/page.tsx
 import { About } from '@/components/forest/About';
 
 export default function Page() {
-  return (
-    <div className='w-full bg-about-page-image bg-cover bg-no-repeat py-10'>
-      <About />
-    </div>
-  );
+  return <About />;
 }
--- src/components/leaf/Header.tsx
+++ src/components/leaf/Header.tsx
@@ -1,4 +1,6 @@
-import { useRouter } from 'next/router';
+'use client';
+
+import { useRouter } from 'next/navigation';
 
 export const Header = () => {
   const router = useRouter();
