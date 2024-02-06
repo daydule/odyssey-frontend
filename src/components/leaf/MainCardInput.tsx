ee4924e8c798685f3dbbb91239c00d380ae6093a
森一貴 <morikazuki@morikazutakanoMacBook-Air.local>
2024-02-07 00:47:02 +0900

    fix: 全角数値が入力できるよう修正・ペンマークと数字の位置調整・カーソル調整

--- src/components/leaf/MainCardInput.tsx
+++ src/components/leaf/MainCardInput.tsx
@@ -1,3 +1,5 @@
+import React, { useRef } from 'react';
+
 type Props = {
   label: string;
   handleChange: React.Dispatch<React.SetStateAction<number>>; // フォームに入力した値を他のフォームに連携するためのSet関数が入る
@@ -8,53 +10,37 @@ type Props = {
 };
 
 const MainCardInput = ({ label, handleChange, value, placeholder, handleClick, isActive }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const result = parseInt(e.target.value, 10) || 0;
+    const convertedValue = e.target.value.replace(/[０-９]/g, (s) => String.fromCharCode(s.charCodeAt(0) - 0xfee0));
+    const result = parseInt(convertedValue, 10) || 0;
     handleChange(result);
   };
-  const inputType = (handleClick: Props['handleClick']) => {
-    if (isActive) {
-      return (
-        <div className='flex'>
-          <input
-            disabled={true}
-            className='text-right outline-none placeholder:text-gray-700'
-            onChange={onChange}
-            value={value || 0}
-            placeholder={value ? String(value) : '入力値'}
-            id={label}
-            type='tel'
-          />
-          <svg
-            onClick={() => handleClick && handleClick(label)}
-            xmlns='http://www.w3.org/2000/svg'
-            width='20'
-            height='20'
-            viewBox='0 0 20 20'
-            fill='none'
-          >
-            <path d='M15 0L12.5 2.5L17.5 7.5L20 5L15 0ZM10 5L0 15V20H5L15 10L10 5Z' fill='#D3D3D3' />
-          </svg>
-        </div>
-      );
-    } else {
-      return (
-        <input
-          className='text-right outline-none'
-          onChange={onChange}
-          placeholder={placeholder}
-          value={value || 0}
-          id={label}
-          type='tel'
-        />
-      );
-    }
+
+  const handleDivClick = () => {
+    if (handleClick) handleClick(label);
+    if (inputRef.current) inputRef.current.focus();
   };
 
   return (
     <div className='flex justify-between border-b-2'>
       <label htmlFor={label}>{label}</label>
-      {inputType(handleClick)}
+      <div className={`flex ${!isActive ? 'cursor-pointer' : ''}`} onClick={handleDivClick}>
+        <input
+          ref={inputRef}
+          className='text-right outline-none placeholder:text-gray-700 mr-2'
+          onChange={onChange}
+          value={value || 0}
+          placeholder={isActive && value ? String(value) : placeholder}
+          id={label}
+          type='tel'
+        />
+        {isActive && (
+          <svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 20 20' fill='none'>
+            <path d='M15 0L12.5 2.5L17.5 7.5L20 5L15 0ZM10 5L0 15V20H5L15 10L10 5Z' fill='#D3D3D3' />
+          </svg>
+        )}
+      </div>
     </div>
   );
 };
