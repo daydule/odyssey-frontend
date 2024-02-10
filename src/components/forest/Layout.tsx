d29c0bb384af67dcbb323a45e759da5e48e6df20
Keisuke Nemura <nemuraystk@gmail.com>
2024-02-10 04:23:48 +0000

    Update Layout component styles

--- src/components/forest/Layout.tsx
+++ src/components/forest/Layout.tsx
@@ -8,12 +8,12 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <div className='no-scrollbar w-full overflow-auto bg-about-page-image bg-cover'>
-      <header className='fixed h-20 w-full'>
+      <header className='fixed z-50 h-20 w-full'>
         <Header />
       </header>
-      <div className='h-screen pt-20'>
+      <div className='relative mt-20 box-border min-h-[calc(100vh-5rem)] pb-8'>
         <section className='py-20'>{children}</section>
-        <footer className='mt-20 w-full'>
+        <footer className='absolute bottom-0 w-full'>
           <Footer />
         </footer>
       </div>
