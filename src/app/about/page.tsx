4859007c72946201c0fb54fb47da99338d037a25
森一貴 <morikazuki@morikazutakanoMacBook-Air.local>
2024-02-21 22:37:51 +0900

    feature: SEO対策

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,9 @@
 import { About } from '@/components/forest/About';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'About',
+};
 
 export default function Page() {
   return <About />;
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,4 +1,9 @@
 import Main from '@/components/forest/Main';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Main',
+};
 
 export default function Page() {
   return <Main />;
