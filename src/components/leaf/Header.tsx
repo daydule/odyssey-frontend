301812e8fd1562416a39354f887a70792393fce2
tom-takeru <tom.2461.ty@gmail.com>
2024-03-03 08:00:21 +0000

    Fix header logo link for mobile devices

--- src/components/leaf/Header.tsx
+++ src/components/leaf/Header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
     <div className='flex h-20 w-full items-center bg-gray-100 py-4'>
       <div
         className='mx-4 h-20 cursor-pointer font-medium md:mx-8'
-        onClick={() => isMobile && router.push('/main')}
+        onClick={() => !isMobile && router.push('/main')}
         data-testid='Header_Logo'
       >
         <img className='h-full w-auto object-contain' src={'/images/header_logo.png'} alt='ヘッダーロゴ画像' />
