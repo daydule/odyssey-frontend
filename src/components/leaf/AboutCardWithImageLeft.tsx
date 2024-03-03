de05f0a5cdf2064d8f71deacc3830c7c66a40cc7
Kota Kawamoto <atok.reverse.1040@gmail.com>
2024-03-03 07:30:01 +0000

    ODY-384 modify デザインを微調整

--- src/components/leaf/AboutCardWithImageLeft.tsx
+++ src/components/leaf/AboutCardWithImageLeft.tsx
@@ -5,7 +5,7 @@ export const AboutCardWithImageLeft = (props: AboutCardWithImageProps) => {
 
   return (
     <div
-      className='flex flex-col rounded-b-3xl border shadow-lg md:h-[384px] md:flex-row md:rounded-[50px] lg:w-[1024px]'
+      className='flex flex-col rounded-b-3xl border shadow-lg md:h-[384px] md:flex-row md:rounded-[50px] lg:h-[612px] lg:w-[1024px]'
       data-testid='AboutCardWithImage'
     >
       <div className='w-full text-center md:w-1/2' data-testid='AboutCardWithImage_ImageArea'>
@@ -17,7 +17,7 @@ export const AboutCardWithImageLeft = (props: AboutCardWithImageProps) => {
       </div>
       <div className='p-4 text-center md:w-1/2 md:p-12' data-testid='AboutCardWithImage_TextArea'>
         <p className='text-xl'>{props.title}</p>
-        <p className='mt-4 text-lg md:mt-12'>{props.text}</p>
+        <p className='mt-4 text-2xl md:mt-12'>{props.text}</p>
       </div>
     </div>
   );
