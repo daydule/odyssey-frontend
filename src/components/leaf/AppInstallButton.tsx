22bf087d548c7fa586d860f9dad67dada4597885
tom-takeru <tom.2461.ty@gmail.com>
2024-03-03 07:52:36 +0000

    Refactor AppInstallButton component

--- src/components/leaf/AppInstallButton.tsx
+++ src/components/leaf/AppInstallButton.tsx
@@ -8,11 +8,7 @@ const AppInstallButton = ({ platform }: Props) => {
   switch (platform) {
     case 'appStore':
       return (
-        <a
-          href='https://apps.apple.com/jp/app/itunes-store/id915061235?itsct=apps_box_badge&itscg=30200'
-          className='inline-block h-20 w-64 overflow-hidden rounded-lg'
-          title='App Store'
-        >
+        <a href='#' className='inline-block h-20 w-64 overflow-hidden rounded-lg' title='App Store'>
           <img
             src='https://tools.applemediaservices.com/api/badges/download-on-the-app-store/black/ja-jp?size=250x83&releaseDate=1464739200&h=b488c8877a1de0c0ee752e2cea473eec'
             alt='Download on the App Store'
@@ -22,11 +18,7 @@ const AppInstallButton = ({ platform }: Props) => {
       );
     case 'googlePlay':
       return (
-        <a
-          href='https://play.google.com/store/#?pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1'
-          className='inline-block h-24 w-56'
-          title='Google Store'
-        >
+        <a href='#' className='inline-block h-24 w-56' title='Google Store'>
           <img
             src='https://play.google.com/intl/en_us/badges/images/generic/ja_badge_web_generic.png'
             alt='Google Play で手に入れよう'
